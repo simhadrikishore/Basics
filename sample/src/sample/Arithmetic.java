a4a91150a2a59c349575efa50cb9a6565457154f
simhadrikishore <74093952+simhadrikishore@users.noreply.github.com>
2020-12-03 13:12:50 +0530

    Arithmetic

---
+++ sample/src/sample/Arithmetic.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a, b;
+		System.out.println("Enter a value of A: ");
+		System.out.println("Enter a value of B: ");
+		Scanner input = new Scanner(System.in);
+		a = input.nextInt();
+		b = input.nextInt();
+		System.out.println("Addition: " + (a + b));
+		System.out.println("Substraction: " + (a - b));
+		System.out.println("Multiplcation: " + (a * b));
+		System.out.println("Division: " + (a / b));
+		System.out.println("Reminder: " + (a % b));
+	}
+}
